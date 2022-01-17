c4dc3d34c7141337296072f88fec27b38493d3f1
Hp <allanokoth695@gmail.com>
2022-01-17 18:17:05 +0300

    add all files

---
+++ src/Interactions.js
@@ -0,0 +1,39 @@
+import {React, useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import styles from './Wallet.module.css'
+
+const Interactions = (props) => {
+
+  const [transferHash, setTransferHash] = useState(null);
+
+  const transferHandler = async (e) => {
+    e.preventDefault();
+    let transferAmount = e.target.sendAmount.value;
+    let receiverAddress = e.target.receiverAddress.value;
+
+    let txt = await props.contract.transfer(receiverAddress, transferAmount);
+    setTransferHash(txt.hash);
+  }
+
+  return (
+    <div className ={styles.interactionsCard}>
+
+      <form onSubmit={transferHandler}>
+        <h3>Transfer Coins</h3>
+        <p> Receiver Address </p>
+        <input type="text" id='receiverAddress' className = {styles.addressInput}/>
+
+        <p> Send Amount </p>
+        <input type="number" id='sendAmount' min='0'/>
+
+        <button type="submit" className={styles.button6}>Send</button>
+        <div>
+          {transferHash}
+        </div>
+      </form>
+
+    </div>
+  );
+}
+
+export default Interactions;
---
+++ src/Wallet.js
@@ -0,0 +1,96 @@
+import {React, useState, useEffect} from 'react'
+import {ethers} from 'ethers'
+import styles from './Wallet.module.css'
+import simple_token_abi from './Contracts/simple_token_abi.json'
+import Interactions from './Interactions'
+
+const Wallet = () => {
+
+  const contractAddress = '0x643Ac92CCe5A2A092297D570d0a4D0C896348275';
+  const [tokenName, setTokenName] = useState("Token");
+  const [connButtonText, setConnButtonText] = useState("Connect Wallet");
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [defaultAccount, setDefaultAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+  const [provider, setProvider] = useState(null);
+  const [signer, setSigner] = useState(null);
+  const [contract, setContract] = useState(null);
+
+
+  const connectWalletHandler = () => {
+    if(window.ethereum && window.ethereum.isMetaMask){
+      window.ethereum.request({method: 'eth_requestAccounts'})
+      .then(result => {
+        accountChangeHandler(result[0]);
+        setConnButtonText('Wallet Connected');
+      })
+      .catch(error => {
+        setErrorMessage(error.message);
+      })
+
+
+    }else{
+      console.log("need to install isMetaMask")
+      setErrorMessage("Please install MetaMask")
+    }
+  }
+
+  const accountChangeHandler = (newAddress) => {
+    setDefaultAccount(newAddress);
+    updateEthers();
+  }
+
+  const updateEthers = () => {
+    let tempProvider = new ethers.providers.Web3Provider(window.ethereum);
+    let tempSigner = tempProvider.getSigner();
+    let tempContract = new ethers.Contract(contractAddress, simple_token_abi, tempSigner)
+  
+    setProvider(tempProvider);
+    setSigner(tempSigner);
+    setContract(tempContract);
+  }
+
+  useEffect(() => {
+    if(contract != null) {
+      updateBalance();
+      updateTokenName();
+    }
+  }, [contract])
+
+  const updateBalance = async () => {
+    let balanceBigN = await contract.balanceOf(defaultAccount);
+    let balanceNumber = balanceBigN.toNumber();
+
+    let decimals = await contract.decimals();
+    let tokenBalance = balanceNumber / Math.pow(10, decimals);
+  
+    setBalance(tokenBalance);
+  }
+
+  const updateTokenName = async () => {
+    setTokenName(await contract.name());
+  }
+
+
+return (
+    <div>
+      <h2> {tokenName + " ERC-20 Wallet"} </h2>
+      <button className = {styles.button6} onClick = {connectWalletHandler}>{connButtonText}</button>
+      
+      <div className = {styles.walletCard}>
+        
+        <div> 
+          <h3>Address: {defaultAccount}</h3>
+        </div>
+
+        <div>
+          <h3>{tokenName} Balance: {balance}</h3>
+        </div>
+        {errorMessage}
+      </div>
+      <Interactions/>
+    </div>
+  );
+}
+
+export default Wallet;
